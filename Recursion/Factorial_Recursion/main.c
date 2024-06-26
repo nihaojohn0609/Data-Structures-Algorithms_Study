a2bdf9f800275666b88ed4aa6563d48b38fa021c
nihaojohn0609 <130543530+nihaojohn0609@users.noreply.github.com>
2024-06-26 13:22:12 +0900

    Add files via upload
    min

---
+++ Recursion/Factorial_Recursion/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fact(int n) {
+    if(n == 0) {
+        return 1;
+    } else {
+        return fact(n - 1) * n;
+    }
+}
+
+int Ifact(int n) {
+    int f = 1;
+    int i;
+    for(i = 1; i <= n; i++) {
+        f = f * i;
+    }
+    return f;
+}
+
+int main(int argc, char **argv) {
+	int r;
+    r =fact(5);
+    printf("%d ", r);
+	return 0;
+}
